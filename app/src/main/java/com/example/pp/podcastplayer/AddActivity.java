2892761622767641c2dbd288dcafb08b6eb1b124
AndrazLogar <lwgar.andraz@gmail.com>
2017-12-27 15:59:46 +0100

    Testni commit

--- app/src/main/java/com/example/pp/podcastplayer/AddActivity.java
+++ app/src/main/java/com/example/pp/podcastplayer/AddActivity.java
@@ -12,6 +12,7 @@ public class AddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // bal bal
     }
 
 }
