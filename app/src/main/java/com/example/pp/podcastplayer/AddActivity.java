8830712e9ff8d8b984cac54aa6162382f6b6ec6d
AndrazLogar <lwgar.andraz@gmail.com>
2017-12-27 17:10:55 +0100

    Dialog za dodajanje RSS

--- app/src/main/java/com/example/pp/podcastplayer/AddActivity.java
+++ app/src/main/java/com/example/pp/podcastplayer/AddActivity.java
@@ -1,10 +1,19 @@
 package com.example.pp.podcastplayer;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
+
+    private String rssURL = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +21,37 @@ public class AddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // bal bal
+
+
+        Button gumbRSS = (Button) findViewById(R.id.button_rss);
+
+        gumbRSS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final EditText txtUrl = new EditText(AddActivity.this);
+
+                txtUrl.setHint("http://");
+
+                new AlertDialog.Builder(AddActivity.this)
+                        .setTitle("Add new podcast")
+                        .setMessage("Please add RSS url feed")
+                        .setView(txtUrl)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                String url = txtUrl.getText().toString();
+                                //todo ob kliku na gumb add se poišče in prenese podcast!
+
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                            }
+                        })
+                        .show();
+            }
+        });
+
     }
 
 }
