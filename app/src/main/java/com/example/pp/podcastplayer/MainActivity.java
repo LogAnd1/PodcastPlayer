d47cad2eac9be1edef1a29abb1d38043a47a3a1d
AndrazLogar <lwgar.andraz@gmail.com>
2017-12-27 15:50:15 +0100

    Sprememba imena aktivnosti

--- app/src/main/java/com/example/pp/podcastplayer/MainActivity.java
+++ app/src/main/java/com/example/pp/podcastplayer/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.pp.podcastplayer;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -77,7 +73,7 @@ public class MainActivity extends AppCompatActivity
         }
         // Odpiranje download zaslona iz menu-ja
         else if (id == R.id.action_add) {
-            Intent intent = new Intent(this, DownloadActivity.class);
+            Intent intent = new Intent(this, AddActivity.class);
             startActivity(intent);
             return true;
         }
