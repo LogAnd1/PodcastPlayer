d965f235345f9add154746a61d925fa9c56adda2
Wick96 <lbasa1@gmail.com>
2018-01-09 20:43:01 +0100

    Fixx

--- app/src/main/java/com/example/pp/podcastplayer/PodcastList.java
+++ app/src/main/java/com/example/pp/podcastplayer/PodcastList.java
@@ -3,18 +3,12 @@ package com.example.pp.podcastplayer;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -44,13 +38,14 @@ public class PodcastList extends AppCompatActivity {
     List<DataRSSmp3> data;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_podcast_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-       // String myString = "";
+
         Intent intent = getIntent();
 
         loadStart();
@@ -81,11 +76,6 @@ public class PodcastList extends AppCompatActivity {
     }
 
     public void loadStart() {
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        setContentView(R.layout.activity_main);
-
-
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
